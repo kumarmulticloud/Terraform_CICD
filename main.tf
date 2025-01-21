05a7dcebaf72f231f55290a667b85a093801133b
Kumar <kumar.multicloud@gmail.com>
2025-01-21 20:09:42 +0530

    jenkins

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "dev" {
+    ami = "ami-0d2614eafc1b0e4d2"
+    instance_type = "t2.medium"
+    tags = {
+      Name = "test"
+    }
+}
